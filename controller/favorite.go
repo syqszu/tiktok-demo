9386de35de7837b599c07bd4be82376c53d43f65
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2023-08-16 20:42:51 +0800

    点赞与点赞列表功能 (#15)
    * 添加点赞和查看点赞列表功能
    
    * 获取点赞列表时校验user_id和token
    
    * 实现取消点赞

--- controller/favorite.go
+++ controller/favorite.go
@@ -1,27 +1,119 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
+	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-// FavoriteAction no practical effect, just check if token is valid
-func FavoriteAction(c *gin.Context) {
-	token := c.Query("token")
+const (
+	FAVOURITE_ACTION_TYPE_FAVOURITE   = 1 // 点赞
+	FAVOURITE_ACTION_TYPE_UNFAVOURITE = 2 // 取消点赞
+)
 
-	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
-	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+// Handles /douyin/favourite/action
+func FavoriteAction(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	token := c.Query("token")
+	videoId, err := strconv.ParseInt(c.Query("video_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, Response{StatusCode: 1, StatusMsg: "Invalid video_id"})
+		return
 	}
+	action, err := strconv.ParseInt(c.Query("action_type"), 10, 32)
+	if err != nil || (action != FAVOURITE_ACTION_TYPE_FAVOURITE && action != FAVOURITE_ACTION_TYPE_UNFAVOURITE) {
+		c.JSON(http.StatusBadRequest, Response{StatusCode: 2, StatusMsg: "Invalid action_type"})
+		return
+	}
+
+	// 检查token是否有效
+	var user User
+	if err := db.First(&user, User{Token: token}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusBadRequest, Response{StatusCode: 3, StatusMsg: "Invalid token"})
+			fmt.Println("Invalid token")
+			return
+		}
+		c.JSON(http.StatusInternalServerError, Response{StatusCode: -1, StatusMsg: "Internal error"})
+		fmt.Println("Internal error")
+		return
+	}
+
+	// 检查video_id是否有效
+	var video Video
+	if err := db.First(&video, Video{Id: videoId}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusBadRequest, Response{StatusCode: 1, StatusMsg: "video_id does not exist"})
+			fmt.Println("Invalid video_id")
+			return
+		}
+		c.JSON(http.StatusInternalServerError, Response{StatusCode: 1, StatusMsg: "Internal error"})
+		fmt.Println("Internal error")
+		return
+	}
+
+	if action == FAVOURITE_ACTION_TYPE_UNFAVOURITE { // 取消点赞
+		// 重复取消点赞时，表中没有对应的video_id，操作会被忽略
+		if err := db.Model(&user).Association("FavoritedVideos").Delete(&video); err != nil {
+			c.JSON(http.StatusInternalServerError, Response{StatusCode: -1, StatusMsg: "Internal error"})
+			fmt.Printf("failed to delete video from favorites: %v", err)
+			return
+		}
+	} else { // 记录点赞
+		// video_id为主键，不会记录重复点赞
+		if err := db.Model(&user).Association("FavoritedVideos").Append(&video); err != nil {
+			c.JSON(http.StatusInternalServerError, Response{StatusCode: -1, StatusMsg: "Internal error"})
+			fmt.Printf("failed to add video to favorites: %v", err)
+		}
+	}
+
+	c.JSON(http.StatusOK, Response{StatusCode: 0})
 }
 
-// FavoriteList all users have same favorite video list
+// Handles /douyin/favourite/list
 func FavoriteList(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	// 获取token和user_id参数
+	token := c.Query("token")
+	userId, err := strconv.ParseInt(c.Query("user_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, Response{StatusCode: 1, StatusMsg: "Invalid user_id"})
+		return
+	}
+
+	// 检查user_id是否有效
+	var user User
+	if err := db.Preload("FavoritedVideos").Where(User{Id: userId}).First(&user).Error; err != nil {
+		// 无效user_id
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusBadRequest, Response{StatusCode: 2, StatusMsg: "user_id does not exist"})
+			fmt.Println("user_id does not exist")
+			return
+		}
+		// 其他错误
+		c.JSON(http.StatusInternalServerError, Response{StatusCode: -1, StatusMsg: "Internal error"})
+		fmt.Println("Internal error")
+		return
+	}
+
+	// 检查token是否有效
+	if user.Token != token {
+		c.JSON(http.StatusBadRequest, Response{StatusCode: 1, StatusMsg: "Invalid token"})
+		fmt.Println("Invalid token")
+		return
+	}
+
+	// 返回点赞列表
 	c.JSON(http.StatusOK, VideoListResponse{
 		Response: Response{
 			StatusCode: 0,
 		},
-		VideoList: DemoVideos,
+		VideoList: user.FavoritedVideos,
 	})
 }
